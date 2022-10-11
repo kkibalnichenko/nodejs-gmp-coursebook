dfb956c362cb1fb5c84bb472fd2d55a14640bb03
Diana Baburina <diana.baburina@photobox.com>
2022-10-11 20:58:50 +0300

    remove code dir

--- Code/5-testing/src/unit-tests/example-1/get-s3-photo-path.ts
+++
@@ -1,6 +0,0 @@
-const getS3PhotoPath = (bucketName: string, photoId: string) => {
-  const base64Encoded = Buffer.from(photoId).toString('base64');
-  return `${bucketName}/${base64Encoded}/${photoId}`;
-};
-
-export default getS3PhotoPath;
